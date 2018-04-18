52aec35e509e02f76030cbe74864ac301d552803
mokawad <31768193+mokawad@users.noreply.github.com>
2018-04-18 01:53:21 -0500

    Create logincontrollertest.java

---
+++ src/test/java/edu/csumb/xtreme/movieticketing/logincontrollertest.java
@@ -0,0 +1,24 @@
+package main.java.edu.csumb.xtreme.movieticketing;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class logincontrollertest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+	
+	}
+
+}
