da9087fe9a54bbb8c7429bb4481f1983b5949d1b
mokawad <31768193+mokawad@users.noreply.github.com>
2018-04-17 19:15:47 -0500

    Update MovieDao.java

--- src/main/java/edu/csumb/xtreme/movieticketing/dao/MovieDao.java
+++ src/main/java/edu/csumb/xtreme/movieticketing/dao/MovieDao.java
@@ -3,8 +3,63 @@ package edu.csumb.xtreme.movieticketing.dao;
 import edu.csumb.xtreme.movieticketing.entities.MovieEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
 
 @Repository
 public interface MovieDao extends JpaRepository<MovieEntity, Integer> {
 
+
+/**
+ * Servlet implementation class MovieList
+ */
+public class MovieList extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MovieList() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<String> list = new ArrayList<String>();
+        String json = null;
+        
+        try {
+			list=AppUtils.listMovies();
+        } catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+        json = new Gson().toJson(list);
+        response.setContentType("application/json");
+        response.getWriter().write(json);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
 }
+
+}
+
+
